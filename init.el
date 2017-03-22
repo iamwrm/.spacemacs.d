38b4b645ac8e95e374742829a32616c5f87e48e8
Ren Wang <iamwrm@gmail.com>
2017-03-22 22:53:32 +0800

    no message

--- init.el
+++ init.el
@@ -133,9 +133,9 @@ values."
    ;; with 2 themes variants, one dark and one light)
 
    dotspacemacs-themes '(
+                         material
                          monokai
                          sanityinc-solarized-dark
-                         material
                          )
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
